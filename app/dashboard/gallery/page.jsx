cab18132aced2e8f164af5b6d277ff510392c67f
Nahid Hossain <nahid4510@gmail.com>
2025-12-25 22:44:13 +0600

    routing

--- app/dashboard/gallery/page.jsx
+++ app/dashboard/gallery/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const page = () => {
+const Page = () => {
     return (
         <div>
-          hello  
+            This is gallery
         </div>
     );
 };
 
-export default page;
+export default Page;
---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const layout = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Gallery</li>
+            </ul>
+        </div>
+    );
+};
+
+export default layout;
